e91ef50e5fb0057afc777914bdfbe989683e0c7c
Srimal Ranga <srimalranga2002@gmail.com>
2026-08-24 12:31:21 +0530

    Replace inline script in index.html with external app.js and add app.js file

---
+++ app.js
@@ -0,0 +1,25 @@
+ let name = "John Doe";
+//  console.log("Hello World");
+//  name="srimal"
+ console.log(name);
+//  alert("Hello " + name);
+
+let age=24;
+let isUnderage=true;
+let customer={}
+let customers= []
+let salary=50000;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isUnderage);
+console.log(typeof customer);
+console.log(typeof customers);
+console.log(typeof salary);
+
+let a = 10;
+
+let b="10";
+
+console.log(a==b);
+console.log(a===b);
