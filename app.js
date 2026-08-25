3ecb7b2edfa6a73546855398de35926796dbeebd
Srimal Ranga <srimalranga2002@gmail.com>
2026-08-25 13:53:35 +0530

    customer object logging

--- app.js
+++ app.js
@@ -1,84 +1,111 @@
- let name = "John Doe";
-//  console.log("Hello World");
-//  name="srimal"
- console.log(name);
-//  alert("Hello " + name);
+//  let name = "John Doe";
+// //  console.log("Hello World");
+// //  name="srimal"
+//  console.log(name);
+// //  alert("Hello " + name);
 
-let age=24;
-let isUnderage=true;
-let customer={}
-let customers= []
-let salary=50000;
+// let age=24;
+// let isUnderage=true;
+// let customer={}
+// let customers= []
+// let salary=50000;
 
-console.log(typeof name);
-console.log(typeof age);
-console.log(typeof isUnderage);
-console.log(typeof customer);
-console.log(typeof customers);
-console.log(typeof salary);
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isUnderage);
+// console.log(typeof customer);
+// console.log(typeof customers);
+// console.log(typeof salary);
 
-let a = 10;
+// let a = 10;
 
-let b="10";
+// let b="10";
 
-console.log(a==b);
-console.log(a===b);
+// console.log(a==b);
+// console.log(a===b);
 
-let x=30;
-let y=20;
+// let x=30;
+// let y=20;
 
 
-if(x>y){
-    console.log("x is greater than y");
+// if(x>y){
+//     console.log("x is greater than y");
+// }
+// else if(x<y){
+//     console.log("x is less than y");
+// }
+
+// else{
+//     console.log("x is equal to y");
+// }
+
+
+// let day=2;
+
+// switch(day){
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday"); 
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// let anser=add(10,20);
+// console.log(anser);
+
+
+// let numbers=[1,"sman",2,3,4,5];
+
+// for(let i=0;i<numbers.length;i++){
+//   console.log(numbers[i]);
+// }
+
+
+
+//Create a Object
+
+let customer1={
+    name:"Srimal",
+    age:24,
+    address:"241 getalawa",
+    salary:180000,
+    items:["item1","item2","item3"],
+    father:{
+        name:"John",
+        age:50,
+        address:"Colombo"
+    }
 }
-else if(x<y){
-    console.log("x is less than y");
-}
 
-else{
-    console.log("x is equal to y");
-}
-
-
-let day=2;
-
-switch(day){
-    case 1:
-        console.log("Monday");
-        break;
-    case 2:
-        console.log("Tuesday"); 
-        break;
-    case 3:
-        console.log("Wednesday");
-        break;
-    case 4:
-        console.log("Thursday");
-        break;
-    case 5:
-        console.log("Friday");
-        break;
-    case 6:
-        console.log("Saturday");
-        break;
-    case 7:
-        console.log("Sunday");
-        break;
-    default:
-        console.log("Invalid day");
-}
-
-
-function add(a,b){
-    return a+b;
-}
-
-let anser=add(10,20);
-console.log(anser);
-
-
-let numbers=[1,"sman",2,3,4,5];
-
-for(let i=0;i<numbers.length;i++){
-  console.log(numbers[i]);
-}
+console.log(customer1.name);
+console.log(customer1.age);
+console.log(customer1.address);
+console.log(customer1.salary);
+console.log(customer1.items);
+console.log(customer1.father.name);
+console.log(customer1.father.age);
+console.log(customer1.father.address);
+console.log(customer1.father);
